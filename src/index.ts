c2b851cbf890f7a7def485ab86dd0ecb3a4b76d0
trongle0504 <104537989+trongle0504@users.noreply.github.com>
2022-11-01 22:02:07 +0700

    Add index.ts for consumers of the API (#10)

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/*********************************************************************
+ * Copyright (c) 2021 Kichwa Coders Canada Inc. and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+
+export * from './AmalgamatorSession';
+export * from './AmalgamatorClient';
+export * from './debugAdapter';
